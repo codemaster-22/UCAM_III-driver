43ee0cc335e7461d3769bba471adfc9aa2d06b9d
Meesa Shivaram Prasad <shivaramprasadmeesa@gmail.com>
2020-11-09 10:06:53 +0000

    updated things

--- camera/ucam.c
+++ camera/ucam.c
@@ -6,6 +6,7 @@ void delay(int time){
 
 void send(const uint8_t* str,int len){
   int i;
+  flush_uart(CAM);
   for(i =0;i<len;i++){
     write_uart_character(CAM,*str);
     str++;
@@ -16,11 +17,9 @@ void recieve_ack(int cmdno,int pid)
 {
   uint8_t str[6]; 
   int i = 0;
-  flush_uart(CAM);
   for(i=0;i<6;i++){
     read_uart_character(CAM,(str+i)); 
   }
-  flush_uart(CAM);
   uint8_t cc = (uint8_t)cmdno; 
   uint8_t pn[] = {(uint8_t)(pid>>8),(uint8_t)(pid%256)};
   uint8_t ack_ex[] = {(uint8_t)(0xAA),(uint8_t)(0x0E),cc,str[3],pn[0],pn[1]};
@@ -47,12 +46,25 @@ void recieve_gen(uint8_t str[],int len)
   }
 }
 
+void recieve_img(uint8_t *ptr){
+  int i;
+  uint8_t str[6];
+  for(i=0;i<6;i++){
+    read_uart_character(CAM,str+i);
+  }
+  for(i=0;i<506;i++){
+    read_uart_character(CAM,ptr+i);
+  }
+}
+
+
 void init_cam()
 {
   int delay_time = 5;
   const uint8_t  sync_command[] = {(uint8_t)(0xAA),(uint8_t)(0x0D), (uint8_t)(0x0),(uint8_t)(0x0),(uint8_t)(0x0),(uint8_t)(0x0)};
   const uint8_t  ack_command[] = {(uint8_t)(0xAA),(uint8_t)(0x0E), (uint8_t)(0x0D),(uint8_t)(0x0),(uint8_t)(0x0),(uint8_t)(0x0)};
-  flush_uart(CAM);
+
+  //SET_BAUDRATE_command : AA 07 31 00 00 00  ( specific for 115200 )
   for (int i = 0;i<60;i++)
   {
     send(sync_command,6);
@@ -75,7 +87,6 @@ void init_cam()
         }
         goto success;
     }
-    end: flush_uart(CAM);
   }
   fail:
     printf("couldn't awake camera : professional sleeper\n");
@@ -86,6 +97,14 @@ void init_cam()
     printf("\n synchronisation process done waiting for 2 seconds for stabilisation \n");
     delay(2);
     printf("\n you are all set \n");
+    const uint8_t  baud_command[] = {(uint8_t)(0xAA),(uint8_t)(0x07), (uint8_t)(0x31),(uint8_t)(0x0),(uint8_t)(0x0),(uint8_t)(0x0)};
+    send(baud_command,6);
+    uint8_t ack_recieve[7];
+    recieve_gen(ack_recieve,6);
+    ack_recieve[6] = 0;
+    printf("\nacknowledgement for baudrate : %s\n",(char *)ack_recieve);
+    flush_uart(CAM);
+    printf("\nbaud rate set set 2 115200 successfully \n")
 }
 
 void get_pic()
@@ -119,9 +138,15 @@ void get_pic()
 
   uint8_t ack_temp[6];
   get_ack(0,0,ack_temp);
+  send(ack_temp, 6);
   int no_of_pg = data[3] + (data[4]<<8) + (data[5]<<16) ;
-  FILE *img;
-  img = fopen("Image.jpg","w");
-  fclose(img) ; 
+  uint8_t *image =(uint8_t*) malloc(no_of_pg);
+  no_of_pg = no_of_pg /(506) ;
+  int i = 0;
+  for(i=0;i<=no_of_pg;i++){
+    recieve_img(image+506*i);
+    get_ack(0,i+1,ack_temp);
+    send(ack_temp, 6);
+  }
 }
 
